2d19f4b605450d02a54da612ce3906cfc8283828
Carlos <61396980+gerardoc96@users.noreply.github.com>
2025-06-09 13:11:32 -0700

    Created integration test
    The test is between the EventList and App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import EventList from './components/EventList'
 import CitySearch from './components/CitySearch'
 import NumberOfEvents from './components/NumberOfEvents'
+import { getEvents } from './api'
 
 const App = () => {
   const [events, setEvents] = useState([]);
   const [numberOfEvents, setNumberOfEvents] = useState('32');
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, numberOfEvents));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
