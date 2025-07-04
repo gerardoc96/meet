24b06e89b6cb9070a4da29ac9357a0e829c79e86
Carlos <61396980+gerardoc96@users.noreply.github.com>
2025-07-04 14:26:40 -0700

    Integrated Atatus to App.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import * as atatus from 'atatus-spa';
+
+atatus.config('6557eb38cd5e4c0fb77189f8a3470817').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
