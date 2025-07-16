c6431d3baa928af01874996d96e7b8eb048c01bb
Carlos <61396980+gerardoc96@users.noreply.github.com>
2025-07-15 20:03:26 -0700

    Adds error alert for invalid number of events

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import EventList from './components/EventList'
 import CitySearch from './components/CitySearch'
 import NumberOfEvents from './components/NumberOfEvents'
 import { getEvents, extractLocations } from './api'
-import { InfoAlert } from './components/Alert'
+import { InfoAlert, ErrorAlert } from './components/Alert'
 
 import './App.css'
 
@@ -14,6 +14,7 @@ const App = () => {
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
+  const [errorAlert, setErrorAlert] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -32,6 +33,7 @@ const App = () => {
     <div className="App">
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
       </div>
       <CitySearch
         allLocations={allLocations}
@@ -40,7 +42,9 @@ const App = () => {
 
       <NumberOfEvents
         numberOfEvents={numberOfEvents}
-        setNumberOfEvents={setNumberOfEvents} />
+        setNumberOfEvents={setNumberOfEvents}
+        setErrorAlert={setErrorAlert}
+      />
 
       <EventList events={events} />
 
--- src/components/NumberOfEvents.jsx
+++ src/components/NumberOfEvents.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 
-const NumberOfEvents = ({ numberOfEvents, setNumberOfEvents }) => {
+const NumberOfEvents = ({ numberOfEvents, setNumberOfEvents, setErrorAlert }) => {
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+
+    if (value === "") {
+      setNumberOfEvents("");
+      setErrorAlert("");
+      return;
+    }
+
+    // Check if the input is a number and positive
+    if (isNaN(value) || parseInt(value) < 1) {
+      setErrorAlert("Please enter a valid number greater than 0.");
+    } else {
+      setErrorAlert("");
+      setNumberOfEvents(value);
+    }
+  };
+
   return (
     <div id="number-of-events">
       <label htmlFor="number-of-events-input">Number of Events:</label>
@@ -8,7 +27,7 @@ const NumberOfEvents = ({ numberOfEvents, setNumberOfEvents }) => {
         id="number-of-events-input"
         type="text"
         value={numberOfEvents}
-        onChange={(e) => setNumberOfEvents(e.target.value)}
+        onChange={handleInputChange}
       />
     </div>
   );
